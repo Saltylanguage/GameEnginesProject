7ec772400fcec917ce09bd515997d8906801af3c
Saltylanguage <joshcampbell1986@gmail.com>
2017-04-08 15:24:28 -0700

    Josh: Map Generator is successfully building a mesh from random data.

---
+++ GameEngines/ThisIsAStickup/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MapGenerator : MonoBehaviour
+{
+
+    int[,] map;
+
+    public int width;
+    public int height;
+
+    [Range(0, 100)]
+    public int randomFillPercent;
+
+    public string seed;
+    public bool useRandomSeed;
+
+    public int smoothIterations;
+
+    void Start()
+    {
+        GenerateMap();
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GenerateMap();
+        }
+    }
+
+    void GenerateMap()
+    {
+        map = new int[width, height];
+        RandomFillMap();
+
+        for (int i = 0; i < smoothIterations; i++)
+        {
+            SmoothMap();
+        }
+
+        MeshGenerator meshGen = GetComponent<MeshGenerator>();
+
+        meshGen.GenerateMesh(map, 1);
+    }
+
+    void RandomFillMap()
+    {
+        if (useRandomSeed)
+        {
+            seed = Time.time.ToString();
+        }
+
+        System.Random pseudoRandom = new System.Random(seed.GetHashCode());
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
+                {
+                    map[x, y] = 1;
+                }
+                else
+                {
+                    map[x, y] = (pseudoRandom.Next(0, 100) < randomFillPercent) ? 1 : 0;
+                }
+            }
+        }
+    }    
+               
+            
+    void SmoothMap()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                int neighborWallCount = GetSurroundingWallCount(x, y);
+
+                if (neighborWallCount > 4)
+                {
+                    map[x, y] = 1;
+                }
+                else if (neighborWallCount < 4)
+                {
+                    map[x, y] = 0;
+                }
+            }
+        }
+    }
+
+
+
+    int GetSurroundingWallCount(int gridX, int gridY)
+    {
+        int wallCount = 0;
+
+        for (int neighborX = gridX - 1; neighborX <= gridX + 1; neighborX++)
+        {
+            for (int neighborY = gridY - 1; neighborY <= gridY + 1; neighborY++)
+            {
+                if (neighborX >= 0 && neighborX < width && neighborY >= 0 && neighborY < height)
+                {
+                    if (neighborX != gridX || neighborY != gridY)
+                    {
+                        wallCount += map[neighborX, neighborY];
+                    }
+
+                }
+                else
+                {
+                    wallCount++;
+                }
+            }
+        }
+
+        return wallCount;
+    }
+
+}
---
+++ GameEngines/ThisIsAStickup/Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,279 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MeshGenerator : MonoBehaviour
+{
+
+    public SquareGrid squareGrid;
+    List<Vector3> vertices;
+    List<int> triangles;
+
+    public void GenerateMesh(int[,] map, float squareSize)
+    {
+        squareGrid = new SquareGrid(map, squareSize);
+
+        vertices = new List<Vector3>();
+        triangles = new List<int>();
+
+        for (int x = 0; x < squareGrid.squares.GetLength(0); x++)
+        {
+            for (int y = 0; y < squareGrid.squares.GetLength(1); y++)
+            {
+                TriangulateSquare(squareGrid.squares[x, y]);
+            }
+        }
+
+        Mesh mesh = new Mesh();
+
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+
+        mesh.RecalculateNormals();
+    }
+
+    void TriangulateSquare(Square square)
+    {
+        switch(square.configuration)
+        {
+            case 0:
+                break;
+            case 1:
+                CreateMesh(square.centreBottom, square.bottomLeft, square.centreLeft);
+                break;
+            case 2:
+                CreateMesh(square.centreRight, square.bottomRight, square.centreBottom);
+                break;
+            case 3:
+                CreateMesh(square.centreRight, square.bottomRight, square.bottomLeft, square.centreLeft);                
+                break;
+            case 4:
+                CreateMesh(square.centreTop, square.topRight, square.centreRight );
+                break;
+            case 5:
+                CreateMesh(square.centreTop, square.topRight, square.centreRight, square.centreBottom, square.bottomLeft, square.centreLeft);
+                break;
+            case 6:
+                CreateMesh(square.centreTop, square.topRight, square.bottomRight, square.centreBottom);
+                break;                            
+            case 7:
+                CreateMesh(square.centreTop, square.topRight, square.bottomRight, square.bottomLeft, square.centreLeft);
+                break;
+            case 8:
+                CreateMesh(square.topLeft, square.centreTop, square.centreLeft);
+                break;
+            case 9:
+                CreateMesh(square.topLeft, square.centreTop, square.centreBottom, square.bottomLeft);
+                break;
+            case 10:
+                CreateMesh(square.topLeft, square.centreTop, square.centreRight, square.bottomRight, square.centreBottom, square.centreLeft);
+                break;
+            case 11:
+                CreateMesh(square.topLeft, square.centreTop, square.centreRight, square.bottomRight, square.bottomLeft);
+                break;
+            case 12:
+                CreateMesh(square.topLeft, square.topRight, square.centreRight, square.centreLeft);
+                break;
+            case 13:
+                CreateMesh(square.topLeft, square.topRight, square.centreRight, square.centreBottom, square.bottomLeft);
+                break;
+            case 14:
+                CreateMesh(square.topLeft, square.topRight, square.bottomRight, square.centreBottom, square.centreLeft);
+                break;
+            case 15:
+                CreateMesh(square.topLeft, square.topRight, square.bottomRight, square.bottomLeft);
+                break;
+        }
+    }
+
+    void CreateMesh(params Node[] points)
+    {
+        AssignVertices(points);
+
+        if (points.Length >= 3)
+        {
+            CreateTriangle(points[0], points[1], points[2]);
+        }
+        if(points.Length >= 4)
+        {
+            CreateTriangle(points[0], points[2], points[3]);
+        }
+        if(points.Length >= 5)
+        {
+            CreateTriangle(points[0], points[3], points[4]);
+        }
+        if(points.Length >= 6)
+        {
+            CreateTriangle(points[0], points[4], points[5]);
+        }
+    }
+
+
+    void AssignVertices(Node[] points)
+    {
+        for(int i =0; i < points.Length; i++)
+        {
+            if(points[i].vertexIndex == -1)
+            {
+                points[i].vertexIndex = vertices.Count;
+                vertices.Add(points[i].position);
+            }
+        }
+    }
+
+    void CreateTriangle(Node a, Node b, Node c)
+    {
+        triangles.Add(a.vertexIndex);
+        triangles.Add(b.vertexIndex);
+        triangles.Add(c.vertexIndex);
+    }
+
+    public class SquareGrid
+    {
+        public Square[,] squares;
+
+        public SquareGrid(int[,] map, float squareSize)
+        {
+            int columns = map.GetLength(0);
+            int rows = map.GetLength(1);
+
+            float mapWidth = columns * squareSize;
+            float mapHeight = rows * squareSize;
+
+            ControlNode[,] controlNodes = new ControlNode[columns, rows];
+
+            for (int x = 0; x < columns; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    Vector3 position = new Vector3(-mapWidth / 2 + x * squareSize + squareSize / 2, 0, -mapHeight / 2 + y * squareSize + squareSize / 2);
+                    controlNodes[x, y] = new ControlNode(position, map[x, y] == 1, squareSize);
+
+                }
+            }
+
+            squares = new Square[columns - 1, rows - 1];
+
+            for (int x = 0; x < columns - 1; x++)
+            {
+                for (int y = 0; y < rows - 1; y++)
+                {
+                    squares[x, y] = new Square(controlNodes[x, y + 1], controlNodes[x + 1, y + 1], controlNodes[x, y], controlNodes[x + 1, y]);
+                }
+            }
+
+        }
+    }
+
+    public class Node
+    {
+        public Vector3 position;
+        public int vertexIndex = -1;
+
+        public Node(Vector3 _pos)
+        {
+            position = _pos;
+        }
+    }
+
+    public class Square
+    {
+        public ControlNode topLeft;
+        public ControlNode topRight;
+        public ControlNode bottomLeft;
+        public ControlNode bottomRight;
+
+        public Node centreLeft;
+        public Node centreRight;
+        public Node centreTop;
+        public Node centreBottom;
+
+        public int configuration;
+
+
+
+        public Square(ControlNode _topLeft, ControlNode _topRight, ControlNode _bottomLeft, ControlNode _bottomRight)
+        {
+            topLeft = _topLeft;
+            topRight = _topRight;
+            bottomLeft = _bottomLeft;
+            bottomRight = _bottomRight;
+
+            centreTop = topLeft.right;
+            centreRight = bottomRight.above;
+            centreBottom = bottomLeft.right;
+            centreLeft = bottomLeft.above;
+
+            if(topLeft.active)
+            {
+                configuration += 8;
+            }
+            if(topRight.active)
+            {
+                configuration += 4;
+            }
+            if(bottomRight.active)
+            {
+                configuration += 2;
+            }
+            if(bottomLeft.active)
+            {
+                configuration += 1;
+            }
+
+        }
+
+    }
+
+    public class ControlNode : Node
+    {
+        public bool active;
+
+        public Node above;
+        public Node right;
+
+        public ControlNode(Vector3 _pos, bool _active, float squareSize) : base(_pos)
+        {
+            active = _active;
+            above = new Node(position + Vector3.forward * squareSize / 2);
+            right = new Node(position + Vector3.right * squareSize / 2);
+        }
+
+    }
+
+
+    //void OnDrawGizmos()
+    //{
+    //    if (squareGrid != null)
+    //    {
+    //        for (int x = 0; x < squareGrid.squares.GetLength(0); x++)
+    //        {
+    //            for (int y = 0; y < squareGrid.squares.GetLength(1); y++)
+    //            {
+
+    //                Gizmos.color = (squareGrid.squares[x, y].topLeft.active) ? Color.black : Color.white;
+    //                Gizmos.DrawCube(squareGrid.squares[x, y].topLeft.position, Vector3.one * .4f);
+
+    //                Gizmos.color = (squareGrid.squares[x, y].topRight.active) ? Color.black : Color.white;
+    //                Gizmos.DrawCube(squareGrid.squares[x, y].topRight.position, Vector3.one * .4f);
+
+    //                Gizmos.color = (squareGrid.squares[x, y].bottomLeft.active) ? Color.black : Color.white;
+    //                Gizmos.DrawCube(squareGrid.squares[x, y].bottomLeft.position, Vector3.one * .4f);
+
+    //                Gizmos.color = (squareGrid.squares[x, y].bottomRight.active) ? Color.black : Color.white;
+    //                Gizmos.DrawCube(squareGrid.squares[x, y].bottomRight.position, Vector3.one * .4f);
+
+    //                Gizmos.color = Color.grey;
+    //                Gizmos.DrawCube(squareGrid.squares[x, y].centreTop.position, Vector3.one * .15f);
+    //                Gizmos.DrawCube(squareGrid.squares[x, y].centreRight.position, Vector3.one * .15f);
+    //                Gizmos.DrawCube(squareGrid.squares[x, y].centreBottom.position, Vector3.one * .15f);
+    //                Gizmos.DrawCube(squareGrid.squares[x, y].centreLeft.position, Vector3.one * .15f);
+    //            }
+    //        }
+    //    }
+    //}
+
+}
+
