1e454592b3d6ea4303ec49d3bade763d08019c3e
Saltylanguage <joshcampbell1986@gmail.com>
2017-04-08 17:21:05 -0700

    Josh:
    Walls mesh creation is working.

--- GameEngines/ThisIsAStickup/Assets/Scripts/MapGenerator.cs
+++ GameEngines/ThisIsAStickup/Assets/Scripts/MapGenerator.cs
@@ -1,22 +1,164 @@
-﻿using UnityEngine;
+﻿//using UnityEngine;
+//using System.Collections;
+//using System;
+
+//public class MapGenerator : MonoBehaviour
+//{
+
+//    int[,] map;
+
+//    public int width;
+//    public int height;
+
+//    [Range(0, 100)]
+//    public int randomFillPercent;
+
+//    public string seed;
+//    public bool useRandomSeed;
+
+//    public int smoothIterations;
+
+//    void Start()
+//    {
+//        GenerateMap();
+//    }
+
+//    void Update()
+//    {
+//        if (Input.GetMouseButtonDown(0))
+//        {
+//            GenerateMap();
+//        }
+//    }
+
+//    void GenerateMap()
+//    {
+//        map = new int[width, height];
+//        RandomFillMap();
+
+//        for (int i = 0; i < smoothIterations; i++)
+//        {
+//            SmoothMap();
+//        }
+
+//        int borderSize = 5;
+//        int[,] borderedMap = new int[width + borderSize * 2, height + borderSize * 2];
+
+//        for (int x = 0; x < borderedMap.GetLength(0); x++)
+//        {
+//            for (int y = 0; y < borderedMap.GetLength(1); y++)
+//            {
+//                if (x >= borderSize && x < width + borderSize && y >= borderSize && y < height + borderSize)
+//                {
+//                    borderedMap[x, y] = map[x - borderSize, y - borderSize];
+//                }
+//                else
+//                {
+//                    borderedMap[x, y] = 1;
+//                }
+//            }
+
+//        }
+
+//        MeshGenerator meshGen = GetComponent<MeshGenerator>();
+
+//        meshGen.GenerateMesh(borderedMap, 1);
+//    }
+
+//    void RandomFillMap()
+//    {
+//        if (useRandomSeed)
+//        {
+//            seed = Time.time.ToString();
+//        }
+
+//        System.Random pseudoRandom = new System.Random(seed.GetHashCode());
+
+//        for (int x = 0; x < width; x++)
+//        {
+//            for (int y = 0; y < height; y++)
+//            {
+//                if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
+//                {
+//                    map[x, y] = 1;
+//                }
+//                else
+//                {
+//                    map[x, y] = (pseudoRandom.Next(0, 100) < randomFillPercent) ? 1 : 0;
+//                }
+//            }
+//        }
+//    }
+
+
+//    void SmoothMap()
+//    {
+//        for (int x = 0; x < width; x++)
+//        {
+//            for (int y = 0; y < height; y++)
+//            {
+//                int neighborWallCount = GetSurroundingWallCount(x, y);
+
+//                if (neighborWallCount > 4)
+//                {
+//                    map[x, y] = 1;
+//                }
+//                else if (neighborWallCount < 4)
+//                {
+//                    map[x, y] = 0;
+//                }
+//            }
+//        }
+//    }
+
+
+
+//    int GetSurroundingWallCount(int gridX, int gridY)
+//    {
+//        int wallCount = 0;
+
+//        for (int neighborX = gridX - 1; neighborX <= gridX + 1; neighborX++)
+//        {
+//            for (int neighborY = gridY - 1; neighborY <= gridY + 1; neighborY++)
+//            {
+//                if (neighborX >= 0 && neighborX < width && neighborY >= 0 && neighborY < height)
+//                {
+//                    if (neighborX != gridX || neighborY != gridY)
+//                    {
+//                        wallCount += map[neighborX, neighborY];
+//                    }
+
+//                }
+//                else
+//                {
+//                    wallCount++;
+//                }
+//            }
+//        }
+
+//        return wallCount;
+//    }
+
+//}
+
+
+using UnityEngine;
 using System.Collections;
 using System;
 
 public class MapGenerator : MonoBehaviour
 {
 
-    int[,] map;
-
     public int width;
     public int height;
-
-    [Range(0, 100)]
-    public int randomFillPercent;
 
     public string seed;
     public bool useRandomSeed;
 
-    public int smoothIterations;
+    [Range(0, 100)]
+    public int randomFillPercent;
+
+    int[,] map;
 
     void Start()
     {
@@ -25,7 +167,7 @@ public class MapGenerator : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             GenerateMap();
         }
@@ -36,15 +178,32 @@ public class MapGenerator : MonoBehaviour
         map = new int[width, height];
         RandomFillMap();
 
-        for (int i = 0; i < smoothIterations; i++)
+        for (int i = 0; i < 5; i++)
         {
             SmoothMap();
         }
 
-        MeshGenerator meshGen = GetComponent<MeshGenerator>();
+        int borderSize = 1;
+        int[,] borderedMap = new int[width + borderSize * 2, height + borderSize * 2];
 
-        meshGen.GenerateMesh(map, 1);
+        for (int x = 0; x < borderedMap.GetLength(0); x++)
+        {
+            for (int y = 0; y < borderedMap.GetLength(1); y++)
+            {
+                if (x >= borderSize && x < width + borderSize && y >= borderSize && y < height + borderSize)
+                {
+                    borderedMap[x, y] = map[x - borderSize, y - borderSize];
+                }
+                else {
+                    borderedMap[x, y] = 1;
+                }
+            }
+        }
+
+        MeshGenerator meshGen = GetComponent<MeshGenerator>();
+        meshGen.GenerateMesh(borderedMap, 1);
     }
+
 
     void RandomFillMap()
     {
@@ -63,61 +222,67 @@ public class MapGenerator : MonoBehaviour
                 {
                     map[x, y] = 1;
                 }
-                else
-                {
+                else {
                     map[x, y] = (pseudoRandom.Next(0, 100) < randomFillPercent) ? 1 : 0;
                 }
             }
         }
-    }    
-               
-            
+    }
+
     void SmoothMap()
     {
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
             {
-                int neighborWallCount = GetSurroundingWallCount(x, y);
+                int neighbourWallTiles = GetSurroundingWallCount(x, y);
 
-                if (neighborWallCount > 4)
-                {
+                if (neighbourWallTiles > 4)
                     map[x, y] = 1;
-                }
-                else if (neighborWallCount < 4)
-                {
+                else if (neighbourWallTiles < 4)
                     map[x, y] = 0;
-                }
+
             }
         }
     }
 
-
-
     int GetSurroundingWallCount(int gridX, int gridY)
     {
         int wallCount = 0;
-
-        for (int neighborX = gridX - 1; neighborX <= gridX + 1; neighborX++)
+        for (int neighbourX = gridX - 1; neighbourX <= gridX + 1; neighbourX++)
         {
-            for (int neighborY = gridY - 1; neighborY <= gridY + 1; neighborY++)
+            for (int neighbourY = gridY - 1; neighbourY <= gridY + 1; neighbourY++)
             {
-                if (neighborX >= 0 && neighborX < width && neighborY >= 0 && neighborY < height)
+                if (neighbourX >= 0 && neighbourX < width && neighbourY >= 0 && neighbourY < height)
                 {
-                    if (neighborX != gridX || neighborY != gridY)
+                    if (neighbourX != gridX || neighbourY != gridY)
                     {
-                        wallCount += map[neighborX, neighborY];
+                        wallCount += map[neighbourX, neighbourY];
                     }
-
                 }
-                else
-                {
+                else {
                     wallCount++;
                 }
             }
         }
 
         return wallCount;
+    }
+
+
+    void OnDrawGizmos()
+    {
+        /*
+        if (map != null) {
+            for (int x = 0; x < width; x ++) {
+                for (int y = 0; y < height; y ++) {
+                    Gizmos.color = (map[x,y] == 1)?Color.black:Color.white;
+                    Vector3 pos = new Vector3(-width/2 + x + .5f,0, -height/2 + y+.5f);
+                    Gizmos.DrawCube(pos,Vector3.one);
+                }
+            }
+        }
+        */
     }
 
 }
--- GameEngines/ThisIsAStickup/Assets/Scripts/MeshGenerator.cs
+++ GameEngines/ThisIsAStickup/Assets/Scripts/MeshGenerator.cs
@@ -6,11 +6,22 @@ public class MeshGenerator : MonoBehaviour
 {
 
     public SquareGrid squareGrid;
+    public MeshFilter walls;
+
     List<Vector3> vertices;
     List<int> triangles;
 
+
+    Dictionary<int, List<Triangle>> triangleDictionary = new Dictionary<int, List<Triangle>>();
+    List<List<int>> outlines = new List<List<int>>();
+    HashSet<int> checkedVertices = new HashSet<int>();
+
     public void GenerateMesh(int[,] map, float squareSize)
     {
+        triangleDictionary.Clear();
+        outlines.Clear();
+        checkedVertices.Clear();
+
         squareGrid = new SquareGrid(map, squareSize);
 
         vertices = new List<Vector3>();
@@ -32,32 +43,69 @@ public class MeshGenerator : MonoBehaviour
         mesh.triangles = triangles.ToArray();
 
         mesh.RecalculateNormals();
+
+        CreateWallMesh();
+                
+    }
+
+    void CreateWallMesh()
+    {
+
+        CalculateMeshOutlines();
+
+        List<Vector3> wallVertices = new List<Vector3>();
+        List<int> wallTriangles = new List<int>();
+        Mesh wallMesh = new Mesh();
+        float wallHeight = 5;
+
+        foreach (List<int> outline in outlines)
+        {
+            for (int i = 0; i < outline.Count - 1; i++)
+            {
+                int startIndex = wallVertices.Count;
+                wallVertices.Add(vertices[outline[i]]); // left
+                wallVertices.Add(vertices[outline[i + 1]]); // right
+                wallVertices.Add(vertices[outline[i]] - Vector3.up * wallHeight); // bottom left
+                wallVertices.Add(vertices[outline[i + 1]] - Vector3.up * wallHeight); // bottom right
+
+                wallTriangles.Add(startIndex + 0);
+                wallTriangles.Add(startIndex + 2);
+                wallTriangles.Add(startIndex + 3);
+
+                wallTriangles.Add(startIndex + 3);
+                wallTriangles.Add(startIndex + 1);
+                wallTriangles.Add(startIndex + 0);
+            }
+        }
+        wallMesh.vertices = wallVertices.ToArray();
+        wallMesh.triangles = wallTriangles.ToArray();
+        walls.mesh = wallMesh;
     }
 
     void TriangulateSquare(Square square)
     {
-        switch(square.configuration)
+        switch (square.configuration)
         {
             case 0:
                 break;
             case 1:
-                CreateMesh(square.centreBottom, square.bottomLeft, square.centreLeft);
+                CreateMesh(square.centreLeft, square.centreBottom, square.bottomLeft);
                 break;
             case 2:
-                CreateMesh(square.centreRight, square.bottomRight, square.centreBottom);
+                CreateMesh(square.bottomRight, square.centreBottom, square.centreRight);
                 break;
             case 3:
-                CreateMesh(square.centreRight, square.bottomRight, square.bottomLeft, square.centreLeft);                
+                CreateMesh(square.centreRight, square.bottomRight, square.bottomLeft, square.centreLeft);
                 break;
             case 4:
-                CreateMesh(square.centreTop, square.topRight, square.centreRight );
+                CreateMesh(square.topRight, square.centreRight, square.centreTop);
                 break;
             case 5:
                 CreateMesh(square.centreTop, square.topRight, square.centreRight, square.centreBottom, square.bottomLeft, square.centreLeft);
                 break;
             case 6:
                 CreateMesh(square.centreTop, square.topRight, square.bottomRight, square.centreBottom);
-                break;                            
+                break;
             case 7:
                 CreateMesh(square.centreTop, square.topRight, square.bottomRight, square.bottomLeft, square.centreLeft);
                 break;
@@ -84,6 +132,10 @@ public class MeshGenerator : MonoBehaviour
                 break;
             case 15:
                 CreateMesh(square.topLeft, square.topRight, square.bottomRight, square.bottomLeft);
+                checkedVertices.Add(square.topLeft.vertexIndex);
+                checkedVertices.Add(square.topRight.vertexIndex);
+                checkedVertices.Add(square.bottomRight.vertexIndex);
+                checkedVertices.Add(square.bottomLeft.vertexIndex);
                 break;
         }
     }
@@ -96,15 +148,15 @@ public class MeshGenerator : MonoBehaviour
         {
             CreateTriangle(points[0], points[1], points[2]);
         }
-        if(points.Length >= 4)
+        if (points.Length >= 4)
         {
             CreateTriangle(points[0], points[2], points[3]);
         }
-        if(points.Length >= 5)
+        if (points.Length >= 5)
         {
             CreateTriangle(points[0], points[3], points[4]);
         }
-        if(points.Length >= 6)
+        if (points.Length >= 6)
         {
             CreateTriangle(points[0], points[4], points[5]);
         }
@@ -113,9 +165,9 @@ public class MeshGenerator : MonoBehaviour
 
     void AssignVertices(Node[] points)
     {
-        for(int i =0; i < points.Length; i++)
+        for (int i = 0; i < points.Length; i++)
         {
-            if(points[i].vertexIndex == -1)
+            if (points[i].vertexIndex == -1)
             {
                 points[i].vertexIndex = vertices.Count;
                 vertices.Add(points[i].position);
@@ -128,6 +180,105 @@ public class MeshGenerator : MonoBehaviour
         triangles.Add(a.vertexIndex);
         triangles.Add(b.vertexIndex);
         triangles.Add(c.vertexIndex);
+
+        Triangle triangle = new Triangle(a.vertexIndex, b.vertexIndex, c.vertexIndex);
+        AddTriangleToDictionary(triangle.vertexIndexA, triangle);
+        AddTriangleToDictionary(triangle.vertexIndexB, triangle);
+        AddTriangleToDictionary(triangle.vertexIndexC, triangle);
+    }
+
+    void AddTriangleToDictionary(int vertexIndexKey, Triangle triangle)
+    {
+        if (triangleDictionary.ContainsKey(vertexIndexKey))
+        {
+            triangleDictionary[vertexIndexKey].Add(triangle);
+        }
+        else
+        {
+            List<Triangle> triangleList = new List<Triangle>();
+            triangleList.Add(triangle);
+            triangleDictionary.Add(vertexIndexKey, triangleList);
+        }
+    }
+
+    void CalculateMeshOutlines()
+    {
+        for (int vertexIndex = 0; vertexIndex < vertices.Count; vertexIndex++)
+        {
+            if (!checkedVertices.Contains(vertexIndex))
+            {
+                int newOutlineVertex = GetConnectedOutlineVertex(vertexIndex);
+                if (newOutlineVertex != -1)
+                {
+                    checkedVertices.Add(vertexIndex);
+
+                    List<int> newOutline = new List<int>();
+
+                    newOutline.Add(vertexIndex);
+                    outlines.Add(newOutline);
+
+                    FollowOutline(newOutlineVertex, outlines.Count - 1);
+                    outlines[outlines.Count - 1].Add(vertexIndex);
+                }
+            }
+        }
+    }
+
+    void FollowOutline(int vertexIndex, int outlineIndex)
+    {
+        outlines[outlineIndex].Add(vertexIndex);
+        checkedVertices.Add(vertexIndex);
+        int nextVertexIndex = GetConnectedOutlineVertex(vertexIndex);
+
+        if (nextVertexIndex != -1)
+        {
+            FollowOutline(nextVertexIndex, outlineIndex);
+        }
+    }
+
+    int GetConnectedOutlineVertex(int vertexIndex)
+    {
+        List<Triangle> trianglesContainingVertex = triangleDictionary[vertexIndex];
+
+        for (int i = 0; i < trianglesContainingVertex.Count; i++)
+        {
+            Triangle triangle = trianglesContainingVertex[i];
+
+            for (int j = 0; j < 3; j++)
+            {
+                int vertexB = triangle[j];
+                if (vertexB != vertexIndex && !checkedVertices.Contains(vertexB))
+                {
+                    if (IsOutlineEdge(vertexIndex, vertexB))
+                    {
+                        return vertexB;
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    bool IsOutlineEdge(int vertexA, int vertexB)
+    {
+        List<Triangle> trianglesContainingVertexA = triangleDictionary[vertexA];
+        int sharedTriangleCount = 0;
+
+        for (int i = 0; i < trianglesContainingVertexA.Count; i++)
+        {
+            if (trianglesContainingVertexA[i].Contains(vertexB))
+            {
+                sharedTriangleCount++;
+                if (sharedTriangleCount > 1)
+                {
+                    break;
+                }
+            }
+        }
+
+
+        return sharedTriangleCount == 1;
     }
 
     public class SquareGrid
@@ -178,6 +329,39 @@ public class MeshGenerator : MonoBehaviour
         }
     }
 
+    struct Triangle
+    {
+        public int vertexIndexA;
+        public int vertexIndexB;
+        public int vertexIndexC;
+        int[] vertices;
+
+        public Triangle(int a, int b, int c)
+        {
+            vertexIndexA = a;
+            vertexIndexB = b;
+            vertexIndexC = c;
+
+            vertices = new int[3];
+            vertices[0] = a;
+            vertices[1] = b;
+            vertices[2] = c;
+        }
+
+        public int this[int i]
+        {
+            get
+            {
+                return vertices[i];
+            }
+        }
+
+        public bool Contains(int vertexIndex)
+        {
+            return vertexIndex == vertexIndexA || vertexIndex == vertexIndexB || vertexIndex == vertexIndexC;
+        }
+    }
+
     public class Square
     {
         public ControlNode topLeft;
@@ -206,19 +390,19 @@ public class MeshGenerator : MonoBehaviour
             centreBottom = bottomLeft.right;
             centreLeft = bottomLeft.above;
 
-            if(topLeft.active)
+            if (topLeft.active)
             {
                 configuration += 8;
             }
-            if(topRight.active)
+            if (topRight.active)
             {
                 configuration += 4;
             }
-            if(bottomRight.active)
+            if (bottomRight.active)
             {
                 configuration += 2;
             }
-            if(bottomLeft.active)
+            if (bottomLeft.active)
             {
                 configuration += 1;
             }
@@ -276,4 +460,3 @@ public class MeshGenerator : MonoBehaviour
     //}
 
 }
-
